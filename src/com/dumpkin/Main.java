662a1079ef393a7e3cb9e86b69b1eaf24f9b2e1f
dumpkin <dumpkin.hm@gmail.com>
2019-04-22 00:21:28 +0300

    Created triangle

--- src/com/dumpkin/Main.java
+++ src/com/dumpkin/Main.java
@@ -1,8 +1,10 @@
+// test
 package com.dumpkin;
 
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        Triangle triangle = new Triangle();
     }
 }
---
+++ src/com/dumpkin/Triangle.java
@@ -0,0 +1,4 @@
+package com.dumpkin;
+
+public class Triangle {
+}
