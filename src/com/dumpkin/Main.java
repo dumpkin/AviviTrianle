38c4bfcd711618589108fce3611ac37e4f41fb18
dumpkin <dumpkin.hm@gmail.com>
2019-04-22 21:52:24 +0300

    realise Triangle methods with parameters and without

--- src/com/dumpkin/Main.java
+++ src/com/dumpkin/Main.java
@@ -1,10 +1,30 @@
-// test
+// class Triangle , в якому буде описано поля: довжина, ширина.
+// Описати дані поля в конструкторі. Створити конструктор з параметрами і без параметрів.
+// Написати методи, які будуть розраховувати площу та периметр.
+// В Main класі продемонструвати створення об’єктів використовуючи два конструктори.
+// Вивести на консоль площу і периметр прямокутника. Вивід на консолі повинен мати наступний вигляд:
+// «Площа прямокутника = тут буде площа », «Периметр прямокутника = тут буде периметр».
+
 package com.dumpkin;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        Triangle triangle = new Triangle();
+        // create object
+
+        float katet1 = 34.4f;
+        float katet2 = 94.1f;
+
+        Triangle triangle1 = new Triangle();
+        Triangle triangle2 = new Triangle();
+        triangle1.setKatets(48.3, 16.8);
+
+        System.out.println("Площа першого трикутника = " + triangle1.getSquareTr());
+        System.out.println("Периметр першого трикутника = " + triangle1.getPerimetrTr());
+        System.out.println("\n____________________________________________");
+        System.out.println("Площа другого трикутника = " + triangle2.calculatSquareTr(katet1, katet2));
+        System.out.println("Периметр другого трикутника = " + triangle2.calculatePerimeter(katet1, katet2));
+
+
     }
 }
--- src/com/dumpkin/Triangle.java
+++ src/com/dumpkin/Triangle.java
@@ -1,4 +1,51 @@
 package com.dumpkin;
 
 public class Triangle {
+
+    private double perimetrTr;
+    private double squareTr;
+    private double katet1;
+    private double katet2;
+
+    //calculate triangle perimetr
+    public double getPerimetrTr() {
+        calcPerim();
+        return perimetrTr;
+    }
+
+    //calculate triangle square
+    public double getSquareTr() {
+        calcSquare();
+        return squareTr;
+    }
+
+    //set katets
+    public void setKatets(double a,double b){
+        this.katet1=a;
+        this.katet2=b;
+    }
+
+
+    //calculate triangle square with parameters
+    public double calculatSquareTr(double katetA, double katetB) {
+        return (katetA * katetB) / 2;
+    }
+
+    //calculate triangle Perimeter with parameters
+    public double calculatePerimeter(double katetA, double katetB) {
+        return katetA + katetB + Math.sqrt((katetA * katetA) + (katetB * katetB));
+    }
+
+
+    //calculate triangle Perimeter without parameters
+    private void calcPerim() {
+        perimetrTr = this.katet1 +  this.katet2 + Math.sqrt( this.katet1 *  this.katet1 +  this.katet2 *  this.katet2);
+    }
+
+    //calculate triangle square without parameters
+    private void calcSquare() {
+        squareTr =  (this.katet1 * this.katet2) / 2;
+
+    }
+
 }
